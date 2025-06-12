9a15c752f2f438e4462c1d6b9686f83b8665a271
Shubham Rana <shubhamrana0108@gmail.com>
2025-06-13 00:42:57 +0530

    backend boiler plate code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,25 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.BASE_URL || 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Welcome to the Home Page!');
+});
+
+export default app;
